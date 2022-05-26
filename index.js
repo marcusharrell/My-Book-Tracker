0f41d5b4a827cd2f6ca0699a93655a348dfb867c
marcusharrell <102714188+marcusharrell@users.noreply.github.com>
2022-05-25 20:44:49 -0400

    Add form and submit button to pop-modal. Once clicked modal closes

--- index.js
+++ index.js
@@ -29,17 +29,29 @@ function addBookToLibrary() {
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector(".trigger");
 const closeButton = document.querySelector(".close-button");
+const submitButton = document.querySelector(".submit-button")
 
+// display modal 
 function toggleModal() {
   modal.classList.toggle("show-modal");
 }
 
+// When the user clicks on the button, open the modal
 function windowOnClick(event) {
   if (event.target === modal) {
     toggleModal();
   }
 }
 
+// when user submits book
+function onSubmit(event) {
+    event.preventDefault();
+    toggleModal()
+
+}
+
+
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+submitButton.addEventListener("click", onSubmit);
