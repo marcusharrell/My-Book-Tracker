3d49b7f0e6050afbce388cd512dbec041902337d
marcusharrell <102714188+marcusharrell@users.noreply.github.com>
2022-05-27 19:11:13 -0400

    Add placeholder cover for books when added to library

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 /* TODO: 
-
-[ ]  Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
-
-[ ] Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
-
-[ ] Add a button on each book’s display to remove the book from the library
-
-    a. You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
-
-[ ] Add a button on each book’s display to change its read status
-    a. To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
-
+[x] - create function that will add book into library. Must be visually displayed onto dom
+[ ] - button to delete book from library
+[x] - array to store library books created from Book constructor
+[ ] - refactor newBookCard function..
 */
-
-// All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. Your code should look something like this:
 
 // ul of all cards
 const cardList = document.querySelector(".cards");
@@ -29,9 +19,8 @@ const Book = function (title, author, pages) {
   return book;
 };
 
-
 // function that adds new book to dom
-const newBookCard = () => {
+const newBookCard = (title, author) => {
   // creates new li element
   card = document.createElement("li");
   // adds "cards_item" classname to li element
@@ -46,6 +35,15 @@ const newBookCard = () => {
 
   card.appendChild(cardDiv); // appending card div to card li
 
+  cardImage = document.createElement("div");
+  cardImage.classList.add("card_image");
+
+  const img = document.createElement("img");
+  img.src = "images/bookph.jpeg";
+
+  cardDiv.appendChild(cardImage);
+  cardImage.appendChild(img);
+
   cardContent = document.createElement("div");
   cardContent.classList.add("card_content");
 
@@ -53,10 +51,14 @@ const newBookCard = () => {
 
   cardTitle = document.createElement("h2"); // creates h2 element
   cardTitle.classList.add("card_title"); // adds 'card_title' class
-  cardTitle.innerHTML = "Moby-Dick";
+  cardTitle.innerHTML = title;
 
+  cardText = document.createElement("p");
+  cardText.classList.add("card_text");
+  cardText.innerHTML = author;
   // finally append book title to card_content div
   cardContent.appendChild(cardTitle);
+  cardContent.appendChild(cardText);
 };
 
 // test books..
@@ -70,17 +72,8 @@ let mobyDick = Book("Moby-Dick", "Herman Melville", 378);
 // library array
 let myLibrary = [hobbit, harryPotter, greatGatsby, mobyDick];
 
-myLibrary.forEach((book) => {
-  for (let key in book) {
-    console.log(`${key}: ${book[key]}`);
-  }
-});
-
-function addBookToLibrary(book) {
-  const cards = document.querySelector(".cards");
-  const cardItem = document.createElement("li");
-  cardItem.appendChild(document.createTextNode(book.title));
-  cards.appendChild(cardItem);
+for (const book of myLibrary) {
+  newBookCard(book.title, book.author);
 }
 
 // popup modal
@@ -111,9 +104,3 @@ trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 submitButton.addEventListener("click", onSubmit);
-
-/* TODO: 
-[ ] - create function that will add book into library. Must be visually displayed onto dom
-[ ] - button to delete book from library
-[x] - array to store library books created from Book constructor
-*/
